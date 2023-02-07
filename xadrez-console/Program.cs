a1bb72b7f2e6e93f58c1cd834a697fd9ebe34fc4
Bernardo <be,zito@hotmail.com>
2023-02-07 11:08:25 -0300

    Criando Classe Tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,11 +4,7 @@ namespace xadrez_console {
     class Program
     {
         static void Main(string[] args) {
-            Posicao p;
-
-            p = new Posicao(3,4);
-
-            Console.WriteLine("Posição: " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
         }
     }
 }
