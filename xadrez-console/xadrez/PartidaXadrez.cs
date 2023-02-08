fede0c24f831b3277814e0eb1844826532262e8d
Bernardo <be,zito@hotmail.com>
2023-02-08 13:47:45 -0300

    Logica de validacao de movimento

--- xadrez-console/xadrez/PartidaXadrez.cs
+++ xadrez-console/xadrez/PartidaXadrez.cs
@@ -6,8 +6,8 @@ namespace xadrez
     internal class PartidaXadrez
     {
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
         public bool terminada { get; private set; }
 
         public PartidaXadrez()
@@ -25,6 +25,49 @@ namespace xadrez
             p.incrementarQteMovimentos();
             Peca pecacapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
+        }
+
+        public void realizaJogada(Posicao origem, Posicao destino)
+        {
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+
+        private void mudaJogador()
+        {
+            if(jogadorAtual == Cor.Branca)
+            {
+                jogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                jogadorAtual = Cor.Branca;
+            }
+        }
+
+        public void validarPosicaoDeOrigem(Posicao pos)
+        {
+            if(tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não existe peça na posição de origem escolhida.");
+            }
+            if(tab.peca(pos).cor != jogadorAtual)
+            {
+                throw new TabuleiroException("A peça de origem escolhida não é sua.");
+            }
+            if(!tab.peca(pos).existeMovimentosPossiveis())
+            {
+                throw new TabuleiroException("Não existem movimentos possíveis para essa peça.");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
+        {
+            if (!tab.peca(origem).podeMoverPara(destino))
+            {
+                throw new TabuleiroException("Posição de destino inválida");
+            }
         }
 
         public void colocarPecas()
